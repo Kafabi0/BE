c986ea499c2e0bfb5d73c25c2a21c997fa46471d
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-08-05 09:40:49 +0700

    docker

--- database/connect.go
+++ database/connect.go
@@ -5,12 +5,16 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"loginapp/models"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=localhost user=postgres password=zerxy dbname=coba port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := os.Getenv("DATABASE_DSN")
+	if dsn == "" {
+		log.Fatal("DATABASE_DSN environment variable not set")
+	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Gagal konek database:", err)
